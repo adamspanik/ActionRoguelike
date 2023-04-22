948eb14978196baf1841972805fbd9a80385b51b
Adam Spanik <adam.spanik@seznam.cz>
2023-04-22 17:16:48 +0200

    L3 assigment using C++

---
+++ Source/ActionRoguelike/Private/RedBarrel.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RedBarrel.h"
+
+
+// Sets default values
+ARedBarrel::ARedBarrel()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComponent");
+	StaticMeshComponent->SetSimulatePhysics(true);
+	StaticMeshComponent->OnComponentHit.AddDynamic(this, &ARedBarrel::ApplyRadialForce);
+	
+	RootComponent = StaticMeshComponent;
+	
+	RadialForceComponent = CreateDefaultSubobject<URadialForceComponent>("RadialForceComponent");
+	RadialForceComponent->Radius = 600.0f;
+	RadialForceComponent->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ARedBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ARedBarrel::ApplyRadialForce(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	RadialForceComponent->FireImpulse();
+}
+
+// Called every frame
+void ARedBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+
+
+
+
---
+++ Source/ActionRoguelike/Public/RedBarrel.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/Actor.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+#include "RedBarrel.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API ARedBarrel : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ARedBarrel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	virtual void ApplyRadialForce(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* StaticMeshComponent;
+	
+	UPROPERTY(EditAnywhere)
+	URadialForceComponent* RadialForceComponent;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+};
