3babfce379cfe370d1c31372d0e2aa5cc4ac0c23
Adam Spanik <adam.spanik@seznam.cz>
2023-05-01 11:59:42 +0200

    L8 DONE

---
+++ Source/ActionRoguelike/Private/STargetDummy.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "STargetDummy.h"
+
+// Sets default values
+ASTargetDummy::ASTargetDummy()
+{
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComponent");
+	RootComponent = StaticMeshComponent;
+
+	AttributeComponent = CreateDefaultSubobject<USAttributeComponent>("AttributeComponent");
+	AttributeComponent->OnHealthChanged.AddDynamic(this, &ASTargetDummy::OnHealthChanged);
+}
+
+void ASTargetDummy::OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComp, float NewHealth, float Delta)
+{
+	if (Delta >= 0)
+		return;
+	
+	StaticMeshComponent->SetScalarParameterValueOnMaterials("TimeToHit", GetWorld()->TimeSeconds);
+}
+
+
---
+++ Source/ActionRoguelike/Public/STargetDummy.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SAttributeComponent.h"
+#include "GameFramework/Actor.h"
+#include "STargetDummy.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API ASTargetDummy : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASTargetDummy();
+
+protected:
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(EditAnywhere)
+	USAttributeComponent* AttributeComponent;
+
+	UFUNCTION()
+	void OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComp, float NewHealth, float Delta);
+};
